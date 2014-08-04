e141605a40ca8420b89247eaf4bc5b384214437b
simoncherian <simoncherian@gmail.com>
2014-08-04 22:46:57 +0530

    Update best.R
    Final best.R file which cleared all the tests in the submit script

--- best.R
+++ best.R
@@ -4,35 +4,37 @@ best <- function(state, outcome){
   outcomes <- c("heart attack", "heart failure", "pneumonia")
   
   states <- unique(outcomeFile[,7])
-    
+  
   if(!(outcome %in% outcomes)) stop("invalid outcome")
   
   if(!(state %in% states)) stop("invalid state")
   
   if (outcome == outcomes[1]){
     outcomeFile[,11] <- as.numeric(outcomeFile[,11])
-    heartAttacks <- outcomeFile[,c(2,11)]
+    heartAttacks <- outcomeFile[,c(2,7,11)]
+    colnames(heartAttacks) <- c("Name", "State", "HeartAttack")
     heartAttacksOmit <- na.omit(heartAttacks)
-    bestH <- lapply(heartAttacksOmit,min)
+    heartAttacksOmit <- heartAttacksOmit[ which(heartAttacksOmit$State == state),]
+    rowNumber <- which.min(heartAttacksOmit$HeartAttack) 
+    return(heartAttacksOmit[rowNumber,]$Name)
     
-    return(unlist(bestH[1][1]))
-  }
-  
-  if (outcome == outcomes[2]){
+  }else if (outcome == outcomes[2]){
     outcomeFile[,17] <- as.numeric(outcomeFile[,17])
-    heartFailures <- outcomeFile[,c(2,17)]
+    heartFailures <- outcomeFile[,c(2,7,17)]
+    colnames(heartFailures) <- c("Name", "State", "HeartFailure")
     heartFailuresOmit <- na.omit(heartFailures)
-    bestH <- lapply(heartFailuresOmit,min)
-    return(unlist(bestH[1][1]))
+    heartFailuresOmit <- heartFailuresOmit[which(heartFailuresOmit$State == state),]
+    rowNumber <- which.min(heartFailuresOmit$HeartFailure)
+    return(heartFailuresOmit[rowNumber,]$Name)
+    
+  }else if (outcome == outcomes[3]){
+    outcomeFile[,23] <- as.numeric(outcomeFile[,23])
+    pneumonia <- outcomeFile[,c(2,7,23)]
+    colnames(pneumonia) <- c("Name", "State", "Pneumonia")
+    pneumoniaOmit <- na.omit(pneumonia)
+    pneumoniaOmit <- pneumoniaOmit[which(pneumoniaOmit$State == state), ]
+    rowNumber <- which.min(pneumoniaOmit$Pneumonia)
+    return(pneumoniaOmit[rowNumber,]$Name)
   }
   
-  if (outcome == outcomes[3]){
-    outcomeFile[,23] <- as.numeric(outcomeFile[,23])
-    pneumonia <- outcomeFile[,c(2,23)]
-    pneumoniaOmit <- na.omit(pneumonia)
-    bestH <- lapply(pneumoniaOmit,min)
-    
-    return(unlist(bestH[1][1]))
-  }
-
 }
